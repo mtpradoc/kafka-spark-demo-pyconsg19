5aa04c07f8f44a520a2a48d2d1f094471d525b69
Marlene <marlene.prado@gmail.com>
2020-06-25 02:19:46 -0500

    Update kafka_consumer_iot_agg_to_console.py

--- iot_demo/kafka_consumer_iot_agg_to_console.py
+++ iot_demo/kafka_consumer_iot_agg_to_console.py
@@ -1,11 +1,11 @@
-# Usage: spark-submit --packages org.apache.spark:spark-sql-kafka-0-10_2.11:2.3.2 kafka_consumer_iot_agg_to_console.py
+# Usage: spark-submit --packages org.apache.spark:spark-sql-kafka-0-10_2.11:2.4.5 kafka_consumer_iot_agg_to_console.py
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import *
 from pyspark.sql.types import *
 from kafka import KafkaConsumer
 
 # Set Kafka config
-kafka_broker_hostname='localhost'
+kafka_broker_hostname='{IP_ADDRESS_OF_KAFKA_SERVER}'
 kafka_consumer_portno='9092'
 kafka_broker=kafka_broker_hostname + ':' + kafka_consumer_portno
 kafka_topic_input='topic-iot-raw'
